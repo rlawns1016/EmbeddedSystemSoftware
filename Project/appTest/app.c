f50811876e7322d1b229a84563304ed693152e6f
Junho Kim <rlawns1016@naver.com>
2019-06-21 15:04:34 +0900

    19.06.21

--- Project/appTest/app.c
+++ Project/appTest/app.c
@@ -3,10 +3,16 @@
 #include <stdlib.h>
 #include <fcntl.h>
 
+#define READ 1
+#define WRITE 2
+#define EXIT 0
+
 int main(void){
 	int fd;
 	int retn;
-	char buf[4] = { 0, 0, 0, 0 };		//4byte
+	int input;
+	int val;
+	unsigned char buf[4] = { 0, 0, 0, 0 };		//4byte
 	
 	fd = open("/dev/usb/usb2-1.2", O_RDWR);
 	if(fd < 0) {
@@ -15,8 +21,37 @@ int main(void){
 	}
     else { printf("[APP] < inter Device has been detected > \n"); }
 	
+	printf("\n*********Simple Test*********\n");
+	printf("*\t1. READ\n");
+	printf("*\t2. WRITE\n");
+	printf("*\t0. EXIT\n");
+	printf("\n*****************************\n");
+	while(1){
+		buf[0] = 0;	//clear buff
+		printf("Input : ");
+		scanf("%d", &input);
+		if(input == EXIT)	break;
+		else if(input == READ){
+			retn = read(fd, buf, 1);
+			val = buf[0];
+			printf("Read Value (1Byte) : %d\n", val);
+		}
+		else if(input == WRITE){
+			printf("Write Value (1 Byte): ");
+			scanf("%d", &val);
+			buf[0] = (unsigned char)val;
+			retn = write(fd, buf, 1);
+			if(retn < 0){
+				printf("Write Error\n");
+				break;
+			}
+		}
+		else{
+			continue;
+		}
+	}
 
-	retn = write(fd, buf, 2);
+	//retn = write(fd, buf, 1);
 
 	close(fd);
 	
